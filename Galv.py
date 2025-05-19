2df61a42b9954402990c7e2ba43c06602bb7bf9d
241RDC004 <68694503+Houlytv@users.noreply.github.com>
2025-05-19 14:52:15 +0300

    Update Galv.py

--- Galv.py
+++ Galv.py
@@ -1,4 +1,4 @@
-from Data_strukt import scrape_news, save_to_csv
+from Scraping import scrape_news, save_to_csv
 from Web_scraping import load_news_by_category, show_news, show_history, save_history_to_csv, load_history_from_csv
 
 def main():
@@ -16,6 +16,7 @@ def main():
     filtered_news = load_news_by_category("news.csv", category)
     show_news(filtered_news)
     save_history_to_csv()
+    
     # 3. Parādīt vēsturi
     choice = input("Rādīt pēdējās ziņas? (j/n): ")
     if choice.lower() == 'j':
